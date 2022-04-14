9760ff4f8f1d2fc5d1ff10c0e3de288ecc9cefce
swastik2000 <swastikm2000@gmail.com>
2022-04-14 23:04:15 +0530

    hashing technique added

--- leetcode/twosum.java
+++ leetcode/twosum.java
@@ -47,3 +47,20 @@ class Solution {
 }
 
 // Hashing method
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    HashMap <Integer,Integer> hm = new HashMap<Integer,Integer>();
+    for(int i=0; i<nums.length;i++){
+        int requiredNumber=target-nums[i];
+        if(hm.containsKey(requiredNumber)){
+            int soln[] ={hm.get(requiredNumber),i};
+                return soln;
+        }
+        hm.put(nums[i],i);
+    }
+        
+       return null; 
+    }
+}
+
